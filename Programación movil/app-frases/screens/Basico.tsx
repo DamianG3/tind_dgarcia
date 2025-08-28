e0ae3d2a86463b9b824ee2ba47f45b7f88ac909b
Damian Garcia <damiangch3@gamil.com>
2025-08-28 17:24:23 -0500

    Practica Frases 1

---
+++ Programación movil/app-frases/screens/Basico.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+// La tarea va aqui
+
+export default function Basico(){
+    return (
+        <View>
+            <Text>Frases Basicas</Text>
+        </View>
+    )
+}
